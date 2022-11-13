f87997211841e3e6af04c0e3f288bdc1e357381d
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-11-13 19:09:44 -0300

    feat: initial state

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
 const UseReducer = (props) => {
+
+    // Aqui criei varios estados iniciasi ficticios que nao vou usar todos, como se fosse um carrinho de compra.
+    // Tudo isso vai ser controlado pelo useReducer.
+    
+    const initialState = {
+        carrinho: [],
+        produtos: [],
+        user: null,
+        number: 0
+    }
+
+
     return (
         <div className="UseReducer">
             <PageTitle
